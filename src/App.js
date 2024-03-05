30c8213d67c17d88a9cae0d8efcafef9f7e92381
JoshwaThomas <joshwajospin2003@gmail.com>
2024-03-05 06:12:37 +0530

    Joshwa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './index.css';
 
 import  Amt from './component/FileUpload';
 
-function App() {
+const App= ()=>{
   return (
+    <div>
+      <h1>CSV FILE UPLOAD</h1>
     < Amt />
+    </div>
   );
 }
 
--- src/component/FileUpload.js
+++ src/component/FileUpload.js
@@ -2,34 +2,34 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const FileUpload = () => {
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState('');
 
-  const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
   };
 
-  const handleFileUpload = async () => {
+  const handleUpload = async () => {
     const formData = new FormData();
-    formData.append('csv', file);
-
+    formData.append('file', file);
     try {
-      const response = await axios.post('/upload', formData, {
+      await axios.post('http://localhost:5000/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      console.log(response.data);
+      alert('File uploaded successfully');
     } catch (error) {
       console.error(error);
+      alert('Error uploading file');
     }
   };
 
   return (
-    <div className= ' pt-32 pl-56'  >
-      <input type="file" accept=".csv" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload CSV file</button>
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
 };
 
-export default FileUpload;
+export default FileUpload;
